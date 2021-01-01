68f0bfa9915643ae239e65eb6dc26fb7ab7d724e
D3M6 <demg.working@gmail.com>
2021-01-01 15:26:33 +0100

    CRUD: recoger valores e insertar valores

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,77 @@
-import logo from './logo.svg';
+import React, {useState, useEffect } from "react";
+import Axios from 'axios';
 import './App.css';
 
 function App() {
 
-  
+  const [movieName, setMoviename] = useState(''); 
+  const [description, setDescription] = useState(''); 
+  const [movieList, setMovieList] = useState([]); 
+
+  const submitAddMovie = () => {
+    Axios.post('http://localhost:3001/api/movie/add', {
+      movieName: movieName, 
+      movieDescription:description
+    }).then(() => {
+      alert("Se ha insertado correctamente.");
+    });
+  };
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/movie/get').then((response) => {
+      setMovieList(response.data);
+    });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>CRUD Application</h1>
       </header>
+
       <div className="form-container p-1">
-        <form className="form">
+        <form className="form" method="POST">
           <div className="form-section">
             <label className="text-light">Name</label>
-            <input type="text" name="movieName"/>
+            <input 
+              type="text" 
+              name="movieName" 
+              onChange={(e)=> {
+                setMoviename(e.target.value)
+              }}
+            />
           </div>
           <div className="form-section">
             <label className="text-light">Description</label>
-            <input type="text" name="movieDescription"/>
+            <input 
+              type="text" 
+              name="movieDescription" 
+              onChange={(e)=> {
+                setDescription(e.target.value)
+              }}
+            />
           </div>
-          <button className="button-input m-1">Submit</button>
+          <button className="button-input m-1" onClick={submitAddMovie}>Submit</button>
         </form>
+      </div>
+      
+      <div className="container p-1">
+        <table>
+          <thead>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+          </thead>
+          <tbody>
+              {movieList.map((val) => {
+                return <tr key={val.id}>
+                          <td>{val.id}</td>
+                          <td>{val.name}</td>
+                          <td>{val.description}</td>
+                        </tr>;
+              })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
@@ -10,15 +12,32 @@ const dataBase = mysql.createPool({
     password:''
 });
 
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.listen(3001, () => {
     console.log('runnig on port 3001');
 });
 
 // Primera ruta creada
 app.get('/',(req,res) => {
-    const sqlInsert = "INSERT INTO movies (name,description) VALUES ('Test','Test')"
-    dataBase.query(sqlInsert,(error, result) =>{
-        //res.send();
-        res.send('Hola mundo, ¿qué tal estás?');
+    res.send('Hola mundo, ¿qué tal estás?');
+});
+
+app.get('/api/movie/get',(req,res) => {
+    const sqlSelect = "SELECT * FROM movies ORDER BY id DESC";
+    dataBase.query(sqlSelect, (error,result) => {
+        res.send(result);
+    });
+});
+
+app.post('/api/movie/add',(req,res) => {
+    const movieName = req.body.movieName;
+    const movieDescription = req.body.movieDescription;
+
+    const sqlInsert = "INSERT INTO movies (name,description) VALUES (?,?)";
+    dataBase.query(sqlInsert, [movieName, movieDescription], (error,result) => {
+        console.log(result);
     });
 });
